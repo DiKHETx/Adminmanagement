9530c64e7fc2d574f46a0369d38af45a7ac557a8
PAKIN JINJAI <63201161@kmitl.ac.th>
2024-03-05 00:51:17 +0700

    Frontend

---
+++ frontend/src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/loginView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "login",
+    component: HomeView,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: function () {
+      return import(/* webpackChunkName: "about" */ "../views/registerView.vue");
+    },
+  },
+  {
+    path: "/home",
+    name: "homePage",
+    component: function () {
+      return import(/* webpackChunkName: "about" */ "../views/homeView.vue");
+    },
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
