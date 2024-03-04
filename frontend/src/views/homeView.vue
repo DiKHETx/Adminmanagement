1f9a9069cacc450b80c4bfecb13364ecafc79029
PAKIN JINJAI <63201161@kmitl.ac.th>
2024-03-05 01:57:02 +0700

    ิbutton

--- frontend/src/views/homeView.vue
+++ frontend/src/views/homeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="max-w-md mx-auto">
+  <form class="max-w-md mx-auto flex flex-col">
     <div class="relative z-0 w-full mb-5 group">
       <input
         type="email"
@@ -111,11 +111,11 @@
       </div>
     </div>
     <button
-      type="submit"
-      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-    >
-      Submit
-    </button>
+    type="submit"
+    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 self-center"
+  >
+    ส่ง
+  </button>
   </form>
   <Table/>
 </template>
