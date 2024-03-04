c2295996238cbd1d171b805af2cf58091074d631
PAKIN JINJAI <63201161@kmitl.ac.th>
2024-03-05 01:01:33 +0700

    Create APIgate.js

---
+++ frontend/src/APIgate.js
@@ -0,0 +1,15 @@
+///Production///
+// const baseURL = "http://161.246.127.114" 
+
+///Development///
+const baseURL = "http://localhost:3000";
+
+const LOGIN_API = "/api/v1/users/login";
+
+const REGISTER_API = "/api/v1/users/register";
+
+export {
+    baseURL,
+    LOGIN_API,
+    REGISTER_API
+}
