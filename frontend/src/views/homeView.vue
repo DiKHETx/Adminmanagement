1a920dff41a855517b8d04816518f5977b5e6e13
PAKIN JINJAI <63201161@kmitl.ac.th>
2024-03-05 01:57:28 +0700

    update

--- frontend/src/views/homeView.vue
+++ frontend/src/views/homeView.vue
@@ -111,13 +111,13 @@
       </div>
     </div>
     <button
-    type="submit"
-    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 self-center"
-  >
-    ส่ง
-  </button>
+      type="submit"
+      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 self-center"
+    >
+      ส่ง
+    </button>
   </form>
-  <Table/>
+  <Table />
 </template>
 
 <script>
@@ -127,7 +127,7 @@ export default {
     return {};
   },
   components: {
-    Table
+    Table,
   },
   methods: {},
 };
