89ef9d497cbf98669891f6937849d27f6b673a85
PAKIN JINJAI <63201161@kmitl.ac.th>
2024-03-05 11:45:32 +0700

    แก้ err
    main.js:6 Feature flag __VUE_PROD_HYDRATION_MISMATCH_DETAILS__ is not explicitly defined. You are running the esm-bundler build of Vue, which expects these compile-time feature flags to be globally injected via the bundler config in order to get better tree-shaking in the production bundle.
    
    For more details, see https://link.vuejs.org/feature-flags.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import './index.css';
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+// เพิ่ม feature flag ให้กับ Vue โดยตรงที่ main.js
+app.config.globalProperties.__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = false;
+
+app.use(router).mount("#app");
