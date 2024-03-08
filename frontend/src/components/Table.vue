8b842d23c5dd9e3e52e112b99445ace96cda0e8e
Satapat123 <63201171@kmitl.ac.th>
2024-03-08 17:19:31 +0700

    new format code and delete space value

--- frontend/src/components/Table.vue
+++ frontend/src/components/Table.vue
@@ -1,55 +1,82 @@
 <template>
   <div>
-    <div class="my-2.5 mx-1 bg-gray-100 relative flex" data-twe-input-wrapper-init data-twe-input-group-ref>
-      <input type="search" v-model="searchQuery"
-  class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary dark:text-black data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
-  placeholder="Search" aria-label="Search" id="exampleFormControlInput" aria-describedby="basic-addon1" />
-
-      <label for="exampleFormControlInput"
-        class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-data-[twe-input-state-active]:-translate-y-[0.9rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary">Search
+    <div
+      class="my-2.5 mx-1 bg-gray-100 relative flex"
+      data-twe-input-wrapper-init
+      data-twe-input-group-ref
+    >
+      <input
+        type="search"
+        v-model="searchQuery"
+        class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary dark:text-black data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
+        placeholder="Search"
+        aria-label="Search"
+        id="exampleFormControlInput"
+        aria-describedby="basic-addon1"
+      />
+      <label
+        for="exampleFormControlInput"
+        class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-data-[twe-input-state-active]:-translate-y-[0.9rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary"
+        >Search
       </label>
-      <button
-        class="relative z-[2] -ms-0.5 flex items-center rounded-e bg-primary px-5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
-        type="button" id="button-addon1" data-twe-ripple-init data-twe-ripple-color="light">
-        <span class="[&>svg]:h-5 [&>svg]:w-5">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-            class="text-neutral-500" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round"
-              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-          </svg>
-        </span>
-      </button>
     </div>
 
     <!-- ตารางแสดงข้อมูล -->
     <table class="min-w-full divide-y divide-gray-200">
       <thead class="bg-gray-50">
         <tr>
-          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th
+            scope="col"
+            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
             อีเมล์
           </th>
-          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th
+            scope="col"
+            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
             ชื่อผู้ใช้
           </th>
-          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th
+            scope="col"
+            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
             ชื่อจริง
           </th>
-          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th
+            scope="col"
+            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
             นามสกุล
           </th>
-          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th
+            scope="col"
+            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
             เบอร์โทร
           </th>
-          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th
+            scope="col"
+            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
             เพศ
           </th>
-          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th
+            scope="col"
+            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
             บทบาท
           </th>
-          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th
+            scope="col"
+            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
             แก้ไข
           </th>
-          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th
+            scope="col"
+            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+          >
             ลบ
           </th>
         </tr>
@@ -78,76 +105,135 @@
             {{ user.role }}
           </td>
           <td class="px-6 py-4 whitespace-nowrap">
-            <button @click="editUser(user)" class="text-indigo-600 hover:text-indigo-900">แก้ไข</button>
+            <button
+              @click="editUser(user)"
+              class="text-indigo-600 hover:text-indigo-900"
+            >
+              แก้ไข
+            </button>
           </td>
           <td class="px-6 py-4 whitespace-nowrap">
-            <button @click="deleteUser(user._id)" class="text-red-600 hover:text-red-900">ลบ</button>
+            <button
+              @click="deleteUser(user._id)"
+              class="text-red-600 hover:text-red-900"
+            >
+              ลบ
+            </button>
           </td>
         </tr>
       </tbody>
     </table>
 
     <!-- Modal สำหรับแก้ไขข้อมูล -->
-    <div v-if="isEditing" class="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-75">
+    <div
+      v-if="isEditing"
+      class="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-75"
+    >
       <div class="bg-white rounded-lg p-8">
         <!-- ฟอร์มสำหรับแก้ไขข้อมูล -->
         <form @submit.prevent="updateUser">
           <!-- ป้อนข้อมูลแก้ไข -->
           Email
-          <input type="text" v-model="editUserData.email" class="border border-gray-300 p-2 mb-4 rounded-lg w-full"
-            placeholder="อีเมล์">
+          <input
+            type="text"
+            v-model="editUserData.email"
+            class="border border-gray-300 p-2 mb-4 rounded-lg w-full"
+            placeholder="อีเมล์"
+          />
           Username
-          <input type="text" v-model="editUserData.username" class="border border-gray-300 p-2 mb-4 rounded-lg w-full"
-            placeholder="ชื่อผู้ใช้">
+          <input
+            type="text"
+            v-model="editUserData.username"
+            class="border border-gray-300 p-2 mb-4 rounded-lg w-full"
+            placeholder="ชื่อผู้ใช้"
+          />
           Firstname
-          <input type="text" v-model="editUserData.firstname" class="border border-gray-300 p-2 mb-4 rounded-lg w-full"
-            placeholder="ชื่อจริง">
+          <input
+            type="text"
+            v-model="editUserData.firstname"
+            class="border border-gray-300 p-2 mb-4 rounded-lg w-full"
+            placeholder="ชื่อจริง"
+          />
           Lastname
-          <input type="text" v-model="editUserData.lastname" class="border border-gray-300 p-2 mb-4 rounded-lg w-full"
-            placeholder="นามสกุล">
+          <input
+            type="text"
+            v-model="editUserData.lastname"
+            class="border border-gray-300 p-2 mb-4 rounded-lg w-full"
+            placeholder="นามสกุล"
+          />
           Phone
-          <input type="text" v-model="editUserData.phoneNo" class="border border-gray-300 p-2 mb-4 rounded-lg w-full"
-            placeholder="เบอร์โทร">
+          <input
+            type="text"
+            v-model="editUserData.phoneNo"
+            class="border border-gray-300 p-2 mb-4 rounded-lg w-full"
+            placeholder="เบอร์โทร"
+          />
           Gender
-          <input type="text" v-model="editUserData.gender" class="border border-gray-300 p-2 mb-4 rounded-lg w-full"
-            placeholder="เพศ">
-
+          <input
+            type="text"
+            v-model="editUserData.gender"
+            class="border border-gray-300 p-2 mb-4 rounded-lg w-full"
+            placeholder="เพศ"
+          />
 
           <!-- ปุ่มยืนยันการแก้ไข -->
-          <button type="submit"
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">บันทึก</button>
+          <button
+            type="submit"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            บันทึก
+          </button>
           <!-- ปุ่มยกเลิกการแก้ไข -->
-          <button @click="cancelEdit"
-            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">ยกเลิก</button>
+          <button
+            @click="cancelEdit"
+            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          >
+            ยกเลิก
+          </button>
         </form>
       </div>
-
     </div>
     <div class="flex justify-between mt-4">
-      <button @click="prevPage" :disabled="currentPage === 1" class="px-4 py-2 bg-gray-200 text-gray-600 rounded-md"
-        :class="{ 'cursor-not-allowed': currentPage === 1 }">ย้อนกลับ</button>
-      <span>หน้า {{ currentPage }} / {{ totalPages }}</span>
-      <button @click="nextPage" :disabled="currentPage === totalPages"
+      <button
+        @click="prevPage"
+        :disabled="currentPage === 1"
         class="px-4 py-2 bg-gray-200 text-gray-600 rounded-md"
-        :class="{ 'cursor-not-allowed': currentPage === totalPages }">ต่อไป</button>
+        :class="{ 'cursor-not-allowed': currentPage === 1 }"
+      >
+        ย้อนกลับ
+      </button>
+      <span>หน้า {{ currentPage }} / {{ totalPages }}</span>
+      <button
+        @click="nextPage"
+        :disabled="currentPage === totalPages"
+        class="px-4 py-2 bg-gray-200 text-gray-600 rounded-md"
+        :class="{ 'cursor-not-allowed': currentPage === totalPages }"
+      >
+        ต่อไป
+      </button>
     </div>
   </div>
 </template>
 
 <script>
-import Swal from 'sweetalert2';
-import axios from 'axios';
-import { DELETEUSERS_API, GETALLUSERS_API, UPDATEUSERS_API, baseURL } from '@/APIgate'; // นำเข้า URL ของ API และ baseURL ผ่านการใช้ module
+import Swal from "sweetalert2";
+import axios from "axios";
+import {
+  DELETEUSERS_API,
+  GETALLUSERS_API,
+  UPDATEUSERS_API,
+  baseURL,
+} from "@/APIgate"; // นำเข้า URL ของ API และ baseURL ผ่านการใช้ module
 
 export default {
   data() {
     return {
       users: [], // ข้อมูลผู้ใช้ที่ได้รับมาจาก API
-      searchQuery: '', // คำค้นหาที่ผู้ใช้ป้อน
+      searchQuery: "", // คำค้นหาที่ผู้ใช้ป้อน
       isEditing: false, // สถานะของการแก้ไขข้อมูลผู้ใช้ผ่าน Modal
       editUserData: {}, // ข้อมูลผู้ใช้ที่ถูกเลือกสำหรับการแก้ไข
       currentPage: 1, // หน้าปัจจุบัน
-      pageSize: 6 // ขนาดของหน้า
+      pageSize: 6, // ขนาดของหน้า
     };
   },
   created() {
@@ -158,7 +244,7 @@ export default {
       if (newVal !== oldVal) {
         this.searchUsers();
       }
-    }
+    },
   },
 
   methods: {
@@ -170,24 +256,12 @@ export default {
         if (this.searchQuery) {
           apiUrl += `?Search=${this.searchQuery}`;
         }
-
         const response = await axios.get(apiUrl);
-
         this.users = response.data.users;
       } catch (error) {
-        console.error('Error fetching users:', error);
+        console.error("Error fetching users:", error);
       }
     },
-
-    // async searchUsers() {
-    //   try {
-    //     const response = await axios.get(`${baseURL}/users/?Search=${this.searchQuery}`);
-    //     this.users = response.data.users;
-    //     console.log('Search successful:', this.users);
-    //   } catch (error) {
-    //     console.error('Error searching users:', error);
-    //   }
-    // },
 
     nextPage() {
       if (this.currentPage < this.totalPages) {
@@ -207,7 +281,7 @@ export default {
         const response = await axios.get(`${baseURL}${GETALLUSERS_API}`); // เรียกใช้ API เพื่อดึงข้อมูลผู้ใช้
         this.users = response.data.users; // กำหนดข้อมูลผู้ใช้ให้กับตัวแปร users
       } catch (error) {
-        console.error('Error fetching users:', error);
+        console.error("Error fetching users:", error);
       }
     },
     editUser(user) {
@@ -217,12 +291,15 @@ export default {
     async updateUser() {
       try {
         const userId = this.editUserData._id; // รหัสผู้ใช้ที่จะถูกแก้ไข
-        const response = await axios.patch(`${baseURL}${UPDATEUSERS_API}/${userId}`, this.editUserData); // ส่งข้อมูลผู้ใช้ที่แก้ไขไปยัง API
-        console.log('Update user:', response.data); // แสดงข้อมูลผู้ใช้ที่ถูกอัปเดตในคอนโซล
+        const response = await axios.patch(
+          `${baseURL}${UPDATEUSERS_API}/${userId}`,
+          this.editUserData
+        ); // ส่งข้อมูลผู้ใช้ที่แก้ไขไปยัง API
+        console.log("Update user:", response.data); // แสดงข้อมูลผู้ใช้ที่ถูกอัปเดตในคอนโซล
         this.isEditing = false; // เปลี่ยนสถานะ isEditing เป็น false เพื่อซ่อน Modal หลังจากการแก้ไขเสร็จสมบูรณ์
         this.fetchUsers(); // ดึงข้อมูลผู้ใช้ใหม่หลังจากการแก้ไขเสร็จสมบูรณ์
       } catch (error) {
-        console.error('Error updating user:', error);
+        console.error("Error updating user:", error);
       }
     },
     cancelEdit() {
@@ -232,18 +309,20 @@ export default {
     async deleteUser(userId) {
       try {
         const { value: confirmDelete } = await Swal.fire({
-          title: 'คุณแน่ใจหรือไม่ว่าต้องการลบผู้ใช้งานนี้?',
-          icon: 'warning',
+          title: "คุณแน่ใจหรือไม่ว่าต้องการลบผู้ใช้งานนี้?",
+          icon: "warning",
           showCancelButton: true,
-          confirmButtonColor: '#d33',
-          cancelButtonColor: '#3085d6',
-          confirmButtonText: 'ตกลง',
-          cancelButtonText: 'ยกเลิก',
+          confirmButtonColor: "#d33",
+          cancelButtonColor: "#3085d6",
+          confirmButtonText: "ตกลง",
+          cancelButtonText: "ยกเลิก",
         });
 
         if (confirmDelete) {
-          const response = await axios.delete(`${baseURL}${DELETEUSERS_API}/${userId}`);
-          console.log('Deleted user:', response.data.user);
+          const response = await axios.delete(
+            `${baseURL}${DELETEUSERS_API}/${userId}`
+          );
+          console.log("Deleted user:", response.data.user);
 
           // Check if a search query is present
           if (this.searchQuery) {
@@ -251,16 +330,13 @@ export default {
             this.fetchUsers();
           } else {
             // Remove the deleted user from the table directly
-            this.users = this.users.filter(user => user._id !== userId);
+            this.users = this.users.filter((user) => user._id !== userId);
           }
         }
       } catch (error) {
-        console.error('Error deleting user:', error);
+        console.error("Error deleting user:", error);
       }
     },
-
-
-
   },
   computed: {
     paginatedUsers() {
@@ -274,17 +350,24 @@ export default {
     },
     filteredUsers() {
       // ฟังก์ชันที่ใช้สำหรับกรองข้อมูลผู้ใช้ตามคำค้นหาของผู้ใช้
-      return this.users.filter(user => {
-        return user.email.toLowerCase().includes(this.searchQuery.toLowerCase()) || // กรองตามอีเมล์
-          user.username.toLowerCase().includes(this.searchQuery.toLowerCase()) || // กรองตามชื่อผู้ใช้
-          user.firstname.toLowerCase().includes(this.searchQuery.toLowerCase()) || // กรองตามชื่อจริง
-          user.lastname.toLowerCase().includes(this.searchQuery.toLowerCase()) || // กรองตามนามสกุล
+      return this.users.filter((user) => {
+        return (
+          user.email.toLowerCase().includes(this.searchQuery.toLowerCase()) || // กรองตามอีเมล์
+          user.username
+            .toLowerCase()
+            .includes(this.searchQuery.toLowerCase()) || // กรองตามชื่อผู้ใช้
+          user.firstname
+            .toLowerCase()
+            .includes(this.searchQuery.toLowerCase()) || // กรองตามชื่อจริง
+          user.lastname
+            .toLowerCase()
+            .includes(this.searchQuery.toLowerCase()) || // กรองตามนามสกุล
           user.phoneNo.toLowerCase().includes(this.searchQuery.toLowerCase()) || // กรองตามเบอร์โทร
           user.gender.toLowerCase().includes(this.searchQuery.toLowerCase()) || // กรองตามเพศ
-          user.role.toLowerCase().includes(this.searchQuery.toLowerCase()); // กรองตามบทบาท
+          user.role.toLowerCase().includes(this.searchQuery.toLowerCase())
+        ); // กรองตามบทบาท
       });
-    }
-  }
+    },
+  },
 };
-
 </script>
--- frontend/src/views/registerView.vue
+++ frontend/src/views/registerView.vue
@@ -1,62 +1,112 @@
 <template>
   <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">สมัครสมาชิก</h2>
+      <h2
+        class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+      >
+        สมัครสมาชิก
+      </h2>
     </div>
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <div>
-        <label class="block text-sm font-medium leading-6 text-gray-900">username</label>
+        <label class="block text-sm font-medium leading-6 text-gray-900"
+          >username</label
+        >
         <div class="mt-2">
-          <input type="text" placeholder="username" v-model="formData.username"
-            class="px-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          <input
+            type="text"
+            placeholder="username"
+            v-model="formData.username"
+            class="px-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
         </div>
       </div>
       <div>
-        <label class="block text-sm font-medium leading-6 text-gray-900">password</label>
+        <label class="block text-sm font-medium leading-6 text-gray-900"
+          >password</label
+        >
         <div class="mt-2">
-          <input type="password" placeholder="password" v-model="formData.password"
-            class="px-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          <input
+            type="password"
+            placeholder="password"
+            v-model="formData.password"
+            class="px-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
         </div>
       </div>
       <div>
-        <label class="block text-sm font-medium leading-6 text-gray-900">First name</label>
+        <label class="block text-sm font-medium leading-6 text-gray-900"
+          >First name</label
+        >
         <div class="mt-2">
-          <input type="text" placeholder="First name" v-model="formData.firstname"
-            class="px-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          <input
+            type="text"
+            placeholder="First name"
+            v-model="formData.firstname"
+            class="px-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
         </div>
       </div>
       <div>
-        <label class="block text-sm font-medium leading-6 text-gray-900">Last name</label>
+        <label class="block text-sm font-medium leading-6 text-gray-900"
+          >Last name</label
+        >
         <div class="mt-2">
-          <input type="text" placeholder="Last name" v-model="formData.lastname"
-            class="px-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          <input
+            type="text"
+            placeholder="Last name"
+            v-model="formData.lastname"
+            class="px-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
         </div>
       </div>
       <div>
-        <label class="block text-sm font-medium leading-6 text-gray-900">Phone</label>
+        <label class="block text-sm font-medium leading-6 text-gray-900"
+          >Phone</label
+        >
         <div class="mt-2">
-          <input type="tel" placeholder="000-0000-000" v-model="formData.phone"
-            class="px-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          <input
+            type="tel"
+            placeholder="000-0000-000"
+            v-model="formData.phone"
+            class="px-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
         </div>
       </div>
       <div>
-        <label class="block text-sm font-medium leading-6 text-gray-900">Email</label>
+        <label class="block text-sm font-medium leading-6 text-gray-900"
+          >Email</label
+        >
         <div class="mt-2">
-          <input type="email" placeholder="user01@test.com" v-model="formData.mail"
-            class="px-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          <input
+            type="email"
+            placeholder="user01@test.com"
+            v-model="formData.mail"
+            class="px-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
         </div>
       </div>
       <div>
-        <label class="block text-sm font-medium leading-6 text-gray-900">gender</label>
+        <label class="block text-sm font-medium leading-6 text-gray-900"
+          >gender</label
+        >
         <div class="mt-2">
-          <input type="text" v-model="formData.gender" placeholder="ชาย,หญิง"
-            class="px-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          <input
+            type="text"
+            v-model="formData.gender"
+            placeholder="ชาย,หญิง"
+            class="px-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
         </div>
       </div>
       <div>
-        <button type="submit" v-on:click="SIGN_UP"
-          class="my-2.5 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-          Sign up </button>
+        <button
+          type="submit"
+          v-on:click="SIGN_UP"
+          class="my-2.5 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Sign up
+        </button>
       </div>
     </div>
   </div>
@@ -64,7 +114,7 @@
 
 <script>
 import axios from "axios";
-import { REGISTER_API, baseURL } from '@/APIgate';
+import { REGISTER_API, baseURL } from "@/APIgate";
 export default {
   name: "SingupView",
   data() {
